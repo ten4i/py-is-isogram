d8cec69fea400d5c27b157aacedeea1bd6c06cec
ten4i <oleksijchunihin@gmail.com>
2026-03-29 23:25:53 +0300

    my solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 def is_isogram(word: str) -> bool:
+    if word == "":
+        return True
+    if not isinstance(word, str):
+        raise TypeError("word will be a string")
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ],
+)
+def test_words_double_and_nondouble(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        (13),
+        (-2),
+        (1.4),
+    ],
+)
+def test_word_types_check(word: str) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word)
